3e970c97c5b66e9f812b1973a0837f480ec017f0
Jay Fernandez <jaypee.1797@gmail.com>
2016-12-06 11:38:21 -0800

    Initial Commit

---
+++ Gamepedia Mobile App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gamepedia Mobile App
+//
+//  Created by Jay on 12/6/16.
+//  Copyright © 2016 Juan Pablo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
